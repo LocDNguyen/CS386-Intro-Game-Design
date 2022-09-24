cf727f668e0dc342aca7127e31609a4ea3630b75
Loc Nguyen <locdacnguyen0@gmail.com>
2022-09-23 21:26:53 -0700

    changed ship movement so it is a constant speed now

--- ship.py
+++ ship.py
@@ -24,6 +24,8 @@ class Ship(Sprite):
 
         self.last_shot = pg.time.get_ticks()
         self.cooldown = 500
+
+        self.speed = 2
     def center_ship(self):
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
@@ -40,8 +42,19 @@ class Ship(Sprite):
         print(f'Ship is dead! Only {self.ships_left} ships left')
         self.game.reset() if self.ships_left > 0 else self.game.game_over()
     def update(self):
-        self.posn += self.vel
-        self.posn, self.rect = clamp(self.posn, self.rect, self.settings)
+        # self.posn += self.vel
+        # self.posn, self.rect = clamp(self.posn, self.rect, self.settings)
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.speed
+        if (keys[pg.K_RIGHT] and self.rect.right < self.settings.screen_width):
+            self.rect.x += self.speed
+        if keys[pg.K_UP] and self.rect.top > 0:
+            self.rect.y -= self.speed
+        if (keys[pg.K_DOWN] and self.rect.bottom < self.settings.screen_height):
+            self.rect.y += self.speed
+
         # if self.shooting:
         #     self.lasers_attempted += 1
         #     if self.lasers_attempted % self.settings.lasers_every == 0:
@@ -49,12 +62,11 @@ class Ship(Sprite):
         #                         ship=self, sound=self.sound)
 
         time_now = pg.time.get_ticks()
-        key = pg.key.get_pressed()
-        if key[pg.K_SPACE] and time_now - self.last_shot > self.cooldown:
+        if keys[pg.K_SPACE] and time_now - self.last_shot > self.cooldown:
             self.lasers.shoot(settings=self.settings, screen=self.screen,
                             ship=self, sound=self.sound)
             self.last_shot = pg.time.get_ticks()
-            
+
         self.draw()
     def draw(self):
         self.screen.blit(self.image, self.rect)
